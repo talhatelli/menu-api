9c05cf86aa5677b1b56721e3cd4eb524c5abdde4
talhatelli <ahmet.telli@tsoft.club>
2023-03-21 22:16:53 +0300

    initial commit

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bracketSpacing: false,
+    jsxBracketSameLine: true,
+    singleQuote: false,
+    trailingComma: "none",
+    printWidth: 120,
+    tabWidth: 2,
+    arrowParens: "avoid"
+  };

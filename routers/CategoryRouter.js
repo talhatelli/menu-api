81e9bd931446bc414545b40b783168b0893058ed
talhatelli <ahmet.telli@tsoft.club>
2023-04-02 14:03:58 +0300

    req.body

--- routers/CategoryRouter.js
+++ routers/CategoryRouter.js
@@ -3,9 +3,8 @@ const router = express.Router();
 
 const Category = require("../models/CategoryModel");
 
-router.get("/", async function (req, res, next) {
-  const category = await Category.create({
-    name: "Yoresel"
-  });
+router.post("/", async function (req, res, next) {
+  const category = await Category.create(req.body);
+  res.send("sonsuz döngü oluyor");
 });
 module.exports = router;

86daf4f5a767cc5757edba6ad022e23495130d6c
Ahmet Talha Telli <ahmet.telli@tsoft.club>
2023-08-02 10:13:42 +0300

    added cors

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const port = process.env.port || 5001;
 const mongoose = require("mongoose");
 const routerIndex = require("./routers/index");
 const morgan = require("morgan");
+const cors = require("cors");
 
 mongoose.set("strictQuery", true);
 mongoose.connect(process.env.MONGO_URI);
 
 app.use(morgan("combined"));
 app.use(express.json());
+app.use(cors());
 app.use("/", routerIndex);
 
 app.listen(port, () => {
